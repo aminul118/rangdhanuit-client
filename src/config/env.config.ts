e5d5286f00d6a7058908361e00e3fddc9e68b5ed
Md Aminul Islam <mdaminulislam@MacBook.local>
2026-04-02 20:20:21 +0600

    Env file config

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,19 +1,20 @@
-const nodeEnv = process.env.NODE_ENV;
-
 const envVars = {
-  nodeEnv,
+  nodeEnv: process.env.NODE_ENV,
   baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
   apiUrl: process.env.NEXT_PUBLIC_API_URL,
+
   analytics: {
     googleAnalytics: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS as string,
     googleTagManagerId: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID as string,
   },
+
   cloudinary: {
     cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME as string,
     apiKey: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY as string,
     apiSecret: process.env.CLOUDINARY_API_SECRET as string,
     uploadPreset: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET as string,
   },
+
   jwt: {
     accessTokenMaxAge: process.env.NEXT_PUBLIC_ACCESS_TOKEN_MAX_AGE,
     refreshTokenMaxAge: process.env.NEXT_PUBLIC_REFRESH_TOKEN_MAX_AGE,
